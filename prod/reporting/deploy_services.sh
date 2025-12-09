e14747a723eda8a98fa44edbefd0ed4c6e69cbfa
Maciej Grochalski <mgrochalski@soldevelo.com>
2025-12-09 16:36:38 +0100

    Upload base reporting deployment configuration

---
+++ prod/reporting/deploy_services.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="52.50.245.193:2376"
+export DOCKER_CERT_PATH="${PWD}/reporting/credentials"
+
+docker volume create pgdata
+
+docker compose down -v --remove-orphans
+docker compose build --no-cache
+docker compose up -d
