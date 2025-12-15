4a9b95a8fc9547958c3516e698f45dae36622726
Maciej Grochalski <mgrochalski@soldevelo.com>
2025-12-15 10:38:35 +0100

    Change DOCKER_HOST

--- prod/reporting/deploy_services.sh
+++ prod/reporting/deploy_services.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="52.50.245.193:2376"
+export DOCKER_HOST="reporting.logimev.cd:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
 docker volume create pgdata
