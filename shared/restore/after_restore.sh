afcc4b9985bf0f69bbf18305a8dd388ed7d5e9a6
Maciej Grochalski <mgrochalski@soldevelo.com>
2026-01-26 17:33:11 +0100

    Fix DB connection in the after_restore.sh script

--- shared/restore/after_restore.sh
+++ shared/restore/after_restore.sh
@@ -12,7 +12,9 @@ set -e
 : ${SUPERSET_SECRET:?"Need to set SUPERSET_SECRET"}
 : ${CLIENT_REDIRECT_URI:?"Need to set CLIENT_REDIRECT_URI"}
 
-CLEAN_URL=$(echo "${DATABASE_URL}" | sed -e 's/^jdbc://')
+
+URL=`echo ${DATABASE_URL} | sed -E 's/^jdbc\:(.+)/\1/'` # jdbc:<url>
+: "${URL:?URL not parsed}"
 
 sql=$(cat <<EOF
 UPDATE auth.auth_users SET password = '${ENCODED_USER_PASSWORD}';
@@ -23,16 +25,9 @@ UPDATE auth.oauth_client_details SET redirecturi = '${CLIENT_REDIRECT_URI}' WHER
 EOF
 )
 
-echo "Connecting to: ${CLEAN_URL} as ${POSTGRES_USER}"
+echo "Connecting to: ${URL} as ${POSTGRES_USER}"
 echo "Executing clearing sensitive data..."
 
-export PGPASSWORD="${POSTGRES_PASSWORD}"
-
-psql "${CLEAN_URL}" -U "${POSTGRES_USER}" -c "SELECT 1" > /dev/null 2>&1 || {
-  echo "Error: Cannot connect to database"
-  exit 1
-}
-
-psql "${CLEAN_URL}" -U "${POSTGRES_USER}" -c "$sql"
+PGPASSWORD="${POSTGRES_PASSWORD}" psql ${URL} -U ${POSTGRES_USER} -c "$sql"
 
 echo "Success: Sensitive data cleared."
