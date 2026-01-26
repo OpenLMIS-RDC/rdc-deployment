6ee8c99e0c6971add00de8751a1875e967766a03
Maciej Grochalski <mgrochalski@soldevelo.com>
2026-01-26 13:37:50 +0100

    Improve deployment

--- prod/deploy_to_env.sh
+++ prod/deploy_to_env.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="www.logimev.cd:2376"
+export DOCKER_HOST="tcp://www.logimev.cd:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
-../shared/restart.sh $1
+$SCRIPT_DIR/../shared/restart_or_restore.sh "$1"
