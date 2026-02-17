caa272c1e7b2ec08d4774d8dc75a388082c39ec3
Maciej Grochalski <mgrochalski@soldevelo.com>
2026-02-17 18:03:40 +0100

    UAT Reporting, changed DB to not use external volume

--- uat/reporting/deploy_services.sh
+++ uat/reporting/deploy_services.sh
@@ -4,7 +4,7 @@ export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://uat-reporting.logimev.cd:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
-docker volume create pgdata
+# docker volume create pgdata
 
 docker compose down -v --remove-orphans
 docker compose build --no-cache
