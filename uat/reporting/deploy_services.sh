8be4be6f330ced5de2e5e21c42171e47b7a3b40d
Maciej Grochalski <mgrochalski@soldevelo.com>
2026-02-17 20:46:46 +0100

    FIX

--- uat/reporting/deploy_services.sh
+++ uat/reporting/deploy_services.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# --- TIMEOUT SETTINGS ---
+export DOCKER_CLIENT_TIMEOUT=300
+export COMPOSE_HTTP_TIMEOUT=300
+
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://uat-reporting.logimev.cd:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
