c97630be984ed6fb94af34857b4c41118882e394
Maciej Grochalski <mgrochalski@soldevelo.com>
2026-02-24 17:25:02 +0100

    Try to increase docker timeout

--- uat/reporting/deploy_services.sh
+++ uat/reporting/deploy_services.sh
@@ -7,5 +7,9 @@ export DOCKER_CERT_PATH="${PWD}/credentials"
 # docker volume create pgdata
 
 docker compose down -v --remove-orphans
+
+export DOCKER_CLIENT_TIMEOUT=1200
+export COMPOSE_HTTP_TIMEOUT=1200
+
 DOCKER_BUILDKIT=0 docker compose build --no-cache
 docker compose up -d
