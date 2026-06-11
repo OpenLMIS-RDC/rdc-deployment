880f6e308ef54167ce812ac07c3d8c8e40959726
Maciej Grochalski <mgrochalski@soldevelo.com>
2026-06-11 12:34:34 +0200

    Unify prod reporting configuration with uat

--- prod/reporting/deploy_services.sh
+++ prod/reporting/deploy_services.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="reporting.logimev.cd:2376"
+export DOCKER_HOST="tcp://reporting.logimev.cd:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
 docker volume create pgdata
